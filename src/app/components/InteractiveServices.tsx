69857744c5afdec23633ed7552f8c87c159eca9e
Your Name <your.email@example.com>
2025-04-10 21:46:53 -0400

    Increase text size in Services section for desktop and laptop views

--- src/app/components/InteractiveServices.tsx
+++ src/app/components/InteractiveServices.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import '../styles/interactive-services.css'
+import '../styles/services-section-updates.css'
 
 interface InteractiveServicesProps {
   heading?: string;
